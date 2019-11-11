8bc78afaedca3e4c0495da3c7efed093de79aac1
indira.saravanan <indira.saravanan@ATMECSINLT-473.ATMECS.CORP>
2019-11-12 01:13:20 +0530

    updates

--- AssessmentTwo/src/test/java/com/atmecs/testscript/TutorialsNinja.java
+++ AssessmentTwo/src/test/java/com/atmecs/testscript/TutorialsNinja.java
@@ -1,5 +1,7 @@
 package com.atmecs.testscript;
 
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -10,42 +12,65 @@ import com.atmecs.utils.ExcelReader;
 import com.atmecs.utils.TestBase;
 
 public class TutorialsNinja extends TestBase {
-	public static ExcelReader readExcel=new ExcelReader(Constants.TUTORIALSNINJATESTDATA_PATH);
+
+	public static ExcelReader readExcel = new ExcelReader(Constants.TUTORIALSNINJATESTDATA_PATH);
 
 	@BeforeTest
 	public void startBrowser() throws Exception {
 		openBrowser();
 	}
 
-	@Test(priority=0)
+	@Test(priority = 0)
 	public void validateHomepage() throws Exception {
-		String actualTitle=driver.getTitle();
-		String expectedTitle=readExcel.getData("validatehomepage", 1, 0);
+		String actualTitle = driver.getTitle();
+		String expectedTitle = readExcel.getData("validatehomepage", 1, 0);
 		Assert.assertEquals(actualTitle, expectedTitle);
 		System.out.println("Homepage is validated");
 	}
-	
-	@Test(priority=1)
-	public void addToCart() throws Exception  {
+
+	@Test(priority = 1)
+	public void addToCart() throws Exception {
 		helper.clickElement(driver, property.properties("loc_macbook_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
-		helper.clearValues(driver, property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
-		helper.sendValues(driver, property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH), "2");
+		helper.clearValues(driver,
+				property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
+		helper.sendValues(driver, property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH),
+				"2");
 		helper.clickElement(driver, property.properties("loc_arttocart_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
 		driver.navigate().back();
 		helper.clickElement(driver, property.properties("loc_iphone_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
-		helper.clearValues(driver, property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
-		helper.sendValues(driver, property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH), "3");
+		helper.clearValues(driver,
+				property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
+		helper.sendValues(driver, property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH),
+				"3");
 		helper.clickElement(driver, property.properties("loc_arttocart_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
 		driver.navigate().back();
 	}
-	
-	@Test(priority=2)
+
+	@Test(priority = 2)
 	public void verifyProductInCart() throws Exception {
 		helper.clickElement(driver, property.properties("loc_carttotal_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
 		helper.clickElement(driver, property.properties("loc_viewcart_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
+		String macbookQuantity = helper.getText(driver,
+				property.properties("loc_macbookquantity_txt", Constants.TUTORIALSNINJALOCATOR_PATH));
+		System.out.println(macbookQuantity);
+		String iphoneQuantity = helper.getText(driver,
+				property.properties("loc_iphonequantity_txt", Constants.TUTORIALSNINJALOCATOR_PATH));
+		System.out.println(iphoneQuantity);
+
 	}
 
-	
+	@Test(priority = 3)
+	public void verifyGrandTotal() throws Exception {
+		String grandTotal = helper.getText(driver,
+				property.properties("loc_grandtotal_txt", Constants.TUTORIALSNINJALOCATOR_PATH));
+		System.out.println(grandTotal);
+		helper.clickElement(driver,
+				property.properties("loc_removecartitem_btn", Constants.TUTORIALSNINJALOCATOR_PATH));
+		String updatedGrandTotal = helper.getText(driver,
+				property.properties("loc_grandtotal_txt", Constants.TUTORIALSNINJALOCATOR_PATH));
+		System.out.println(updatedGrandTotal);
+	}
+
 	@AfterTest
 	public void endBrowser() {
 		closeBrowser();
