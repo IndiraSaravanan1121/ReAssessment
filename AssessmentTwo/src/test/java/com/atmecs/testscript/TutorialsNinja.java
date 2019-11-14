1cd06d133c41ef7586924b774dfc6ca85493240e
indira.saravanan <indira.saravanan@ATMECSINLT-473.ATMECS.CORP>
2019-11-14 11:12:55 +0530

    updated

--- AssessmentTwo/src/test/java/com/atmecs/testscript/TutorialsNinja.java
+++ AssessmentTwo/src/test/java/com/atmecs/testscript/TutorialsNinja.java
@@ -47,21 +47,18 @@ public class TutorialsNinja extends TestBase {
 		helper.clickElement(driver, property.properties("loc_search_txtbx", Constants.TUTORIALSNINJALOCATOR_PATH));
 		helper.sendValues(driver, property.properties("loc_search_txtbx", Constants.TUTORIALSNINJALOCATOR_PATH),
 				readExcel.getData("tutorialsninja", 1, 1));
-		//helper.sendEnter(driver, property.properties("loc_search_txtbx", Constants.TUTORIALSNINJALOCATOR_PATH));
 		helper.clickElement(driver, property.properties("loc_macbook_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
 		helper.clearValues(driver,
 				property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
-		String macbookQuantity=readExcel.getData("tutorialsninja", 2, 1);
+		String macbookQuantity = readExcel.getData("tutorialsninja", 2, 1);
 		helper.sendValues(driver, property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH),
 				macbookQuantity);
 		helper.clickElement(driver, property.properties("loc_addtocart_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
-		driver.navigate().back();
+		helper.clickElement(driver, property.properties("loc_homepage_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
 		helper.clickElement(driver, property.properties("loc_search_txtbx", Constants.TUTORIALSNINJALOCATOR_PATH));
-		
 		helper.sendValues(driver, property.properties("loc_search_txtbx", Constants.TUTORIALSNINJALOCATOR_PATH),
 				readExcel.getData("tutorialsninja", 1, 2));
 		helper.sendEnter(driver, property.properties("loc_search_txtbx", Constants.TUTORIALSNINJALOCATOR_PATH));
-		helper.clickElement(driver, property.properties("loc_macbook_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
 		helper.clickElement(driver, property.properties("loc_iphone_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
 		helper.clearValues(driver,
 				property.properties("loc_productsquantity_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
@@ -72,47 +69,48 @@ public class TutorialsNinja extends TestBase {
 
 	}
 
-	/*
-	 * @Test(priority = 3) public void verifyProductInCart() throws Exception {
-	 * log.info("3.Verify product available in the cart");
-	 * helper.clickElement(driver, property.properties("loc_carttotal_lbl",
-	 * Constants.TUTORIALSNINJALOCATOR_PATH)); helper.clickElement(driver,
-	 * property.properties("loc_viewcart_lbl",
-	 * Constants.TUTORIALSNINJALOCATOR_PATH)); String macbookQuantity =
-	 * helper.getText(driver, property.properties("loc_macbookquantity_txt",
-	 * Constants.TUTORIALSNINJALOCATOR_PATH)); System.out.println(macbookQuantity);
-	 * String iphoneQuantity = helper.getText(driver,
-	 * property.properties("loc_iphonequantity_txt",
-	 * Constants.TUTORIALSNINJALOCATOR_PATH)); System.out.println(iphoneQuantity);
-	 * 
-	 * }
-	 * 
-	 * @Test(priority = 4) public void validateProductDetail() throws Exception {
-	 * log.info("4.Validate product details"); helper.clickElement(driver,
-	 * property.properties("loc_macbook_lbl",
-	 * Constants.TUTORIALSNINJALOCATOR_PATH)); String actualAvailability =
-	 * helper.getText(driver, property.properties("loc_productavailability_txt",
-	 * Constants.TUTORIALSNINJALOCATOR_PATH)); String expectedAvailability =
-	 * readExcel.getData("validateproductdetail", 1, 0);
-	 * Assert.assertEquals(actualAvailability, expectedAvailability); String
-	 * actualPriceAndTax = helper.getText(driver,
-	 * property.properties("loc_productpriceandtax_txt",
-	 * Constants.TUTORIALSNINJALOCATOR_PATH)); String expectedPriceAndTax =
-	 * readExcel.getData("validateproductdetail", 1, 0);
-	 * Assert.assertEquals(actualPriceAndTax, expectedPriceAndTax);
-	 * 
-	 * }
-	 * 
-	 * @Test(priority = 5) public void verifyGrandTotal() throws Exception {
-	 * log.info("5.Verify grant total and its update"); String grandTotal =
-	 * helper.getText(driver, property.properties("loc_grandtotal_txt",
-	 * Constants.TUTORIALSNINJALOCATOR_PATH)); System.out.println(grandTotal);
-	 * helper.clickElement(driver, property.properties("loc_removecartitem_btn",
-	 * Constants.TUTORIALSNINJALOCATOR_PATH)); String updatedGrandTotal =
-	 * helper.getText(driver, property.properties("loc_grandtotal_txt",
-	 * Constants.TUTORIALSNINJALOCATOR_PATH));
-	 * System.out.println(updatedGrandTotal); }
-	 */
+	@Test(priority = 3)
+	public void verifyProductInCart() throws Exception {
+		log.info("3.Verify product available in the cart");
+		helper.clickElement(driver, property.properties("loc_carttotal_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
+		helper.clickElement(driver, property.properties("loc_viewcart_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
+		String macbookQuantity = helper.getText(driver,
+				property.properties("loc_macbookquantity_txt", Constants.TUTORIALSNINJALOCATOR_PATH));
+		System.out.println(macbookQuantity);
+		String iphoneQuantity = helper.getText(driver,
+				property.properties("loc_iphonequantity_txt", Constants.TUTORIALSNINJALOCATOR_PATH));
+		System.out.println(iphoneQuantity);
+
+	}
+
+	@Test(priority = 4)
+	public void validateProductDetail() throws Exception {
+		log.info("4.Validate product details");
+		helper.clickElement(driver, property.properties("loc_macbook_lbl", Constants.TUTORIALSNINJALOCATOR_PATH));
+		String actualAvailability = helper.getText(driver,
+				property.properties("loc_productavailability_txt", Constants.TUTORIALSNINJALOCATOR_PATH));
+		String expectedAvailability = readExcel.getData("validateproductdetail", 1, 0);
+		Assert.assertEquals(actualAvailability, expectedAvailability);
+		String actualPriceAndTax = helper.getText(driver,
+				property.properties("loc_productpriceandtax_txt", Constants.TUTORIALSNINJALOCATOR_PATH));
+		String expectedPriceAndTax = readExcel.getData("validateproductdetail", 1, 0);
+		Assert.assertEquals(actualPriceAndTax, expectedPriceAndTax);
+
+	}
+
+	@Test(priority = 5)
+	public void verifyGrandTotal() throws Exception {
+		log.info("5.Verify grant total and its update");
+		String grandTotal = helper.getText(driver,
+				property.properties("loc_grandtotal_txt", Constants.TUTORIALSNINJALOCATOR_PATH));
+		System.out.println(grandTotal);
+		helper.clickElement(driver,
+				property.properties("loc_removecartitem_btn", Constants.TUTORIALSNINJALOCATOR_PATH));
+		String updatedGrandTotal = helper.getText(driver,
+				property.properties("loc_grandtotal_txt", Constants.TUTORIALSNINJALOCATOR_PATH));
+		System.out.println(updatedGrandTotal);
+	}
+
 	@AfterTest
 	public void endBrowser() {
 		try {
